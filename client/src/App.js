98ae3c37199fb15d5cbed407db008c3281d9dfe4
janeishahu <janeishahu@aol.com>
2021-10-17 19:33:10 -0400

    installed react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './index.css';
-import Addmakeup from './components/makeup/Addmakeup'
+import Addmakeup from './components/makeup/Addmakeup';
+import MakeupLook from './components/makeup/MakeupLook';
 
 function App() {
+  const [makeupLook, setMakeupLook] = useState([]);
+
+  const addMakeupHandler = (foundation, eShadow, eLiner, blush, lStick) => {
+    setMakeupLook((prevMakeupLook) => {
+      return [
+        ...prevMakeupLook,
+        {
+          foundation: foundation,
+          eyeshadow: eShadow,
+          eyeliner: eLiner,
+          blush: blush,
+          lipstick: lStick,
+          id: Math.random().toString()
+        },
+      ];
+    });
+  };
+
   return (
-    <Addmakeup/>
-  )
-  
+    <div>
+      <Addmakeup onAddMakeup={addMakeupHandler} />
+      <MakeupLook makeup={makeupLook} />
+    </div>
+  );
 }
 
 export default App;
