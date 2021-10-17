5caa56f728dfab5a1a13e128c01416871f45f5a4
janeishahu <janeishahu@aol.com>
2021-10-17 12:20:57 -0400

    setting up forms and inputs

---
+++ client/src/components/makeup/Addmakeup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Addmakeup = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor='foundation'></label>
+        <input id='foundation' type='text' />
+        <label htmlFor='eyeshadow'></label>
+        <input id='eyeshadow' type='text' />
+        <label htmlFor='eyeliner'></label>
+        <input id='eyeliner' type='text' />
+        <label htmlFor='blush'></label>
+        <input id='blush' type='text' />
+        <label htmlFor='lipstick'></label>
+        <input id='stick' type='text' />
+        <button />
+      </form>
+    </div>
+  );
+};
+
+export default Addmakeup;
