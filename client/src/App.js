f99b38402c79a35dcc259396ebeef17617b67e3c
janeishahu <janeishahu@aol.com>
2021-10-17 13:29:17 -0400

    added handlers for inputs

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css';
+import Addmakeup from './components/makeup/Addmakeup'
+
+function App() {
+  return (
+    <Addmakeup/>
+  )
+  
+}
+
+export default App;
--- client/src/components/makeup/Addmakeup.js
+++ client/src/components/makeup/Addmakeup.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import classes from './Addmakeup.module.css';
+import Button from '../UI/Button';
 
-const Addmakeup = () => {
+const Addmakeup = (props) => {
+  const [enteredFoundation, setEnteredFoundation] = useState('');
+  const [enteredEyeshadow, setEnteredEyeshadow] = useState('');
+  const [enteredEyeliner, setEnteredEyeliner] = useState('');
+  const [enteredBlush, setEnteredBlush] = useState('');
+  const [enteredLipstick, setEnteredLipstick] = useState('');
+
+  const addMakeupHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const foundationChangeHandler = (event) => {
+    setEnteredFoundation(event.target.value);
+  };
+
+  const eyeshadowChangeHandler = (event) => {
+    setEnteredEyeshadow(event.target.value);
+  };
+
+  const eyelinerChangeHandler = (event) => {
+    setEnteredEyeliner(event.target.value);
+  };
+
+  const blushChangeHandler = (event) => {
+    setEnteredBlush(event.target.value);
+  };
+
+  const lipstickChangeHandler = (event) => {
+    setEnteredLipstick(event.target.value);
+  };
   return (
-    <div>
-      <form>
-        <label htmlFor='foundation'></label>
-        <input id='foundation' type='text' />
-        <label htmlFor='eyeshadow'></label>
-        <input id='eyeshadow' type='text' />
-        <label htmlFor='eyeliner'></label>
-        <input id='eyeliner' type='text' />
-        <label htmlFor='blush'></label>
-        <input id='blush' type='text' />
-        <label htmlFor='lipstick'></label>
-        <input id='stick' type='text' />
-        <button />
+    <Card className={classes.input}>
+      <form onSubmit={addMakeupHandler}>
+        <label htmlFor='Foundation'>Foundation</label>
+        <input id='Foundation' type='text' onChange={foundationChangeHandler} />
+        <label htmlFor='Eyeshadow'>Eyeshadow</label>
+        <input id='Eyeshadow' type='text' onChange={eyeshadowChangeHandler} />
+        <label htmlFor='Eyeliner'>Eyeliner</label>
+        <input id='Eyeliner' type='text' onChange={eyelinerChangeHandler} />
+        <label htmlFor='Blush'>Blush</label>
+        <input id='Blush' type='text' onChange={blushChangeHandler} />
+        <label htmlFor='Lipstick'>Lipstick</label>
+        <input id='Lipstick' type='text' onChange={lipstickChangeHandler} />
+        <Button type='submit'>Add Makeup</Button>
       </form>
-    </div>
+    </Card>
   );
 };
 
