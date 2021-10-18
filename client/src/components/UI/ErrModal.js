c3bd660ac427e9c3ed3fee0f7de8cb05dd0d10ac
janeishahu <janeishahu@aol.com>
2021-10-18 01:34:41 -0400

    added modal and refactored code in AddMakeup component

---
+++ client/src/components/UI/ErrModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrModal.module.css';
+
+const ErrModal = (props) => {
+  return (<>
+      <div className={classes.backdrop} onClick={props.onHandleErr}></div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onHandleErr}>Okay</Button>
+      </footer>
+    </Card> </>
+  );
+};
+
+export default ErrModal;
