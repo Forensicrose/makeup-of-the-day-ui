8f8f2a0ca4c01427b0374a1c3bc5b9ae27c232b2
janeishahu <janeishahu@aol.com>
2021-10-19 12:58:00 -0400

    added go back button and other features

---
+++ client/src/components/GoBack/GoBack.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => ( <NavLink to ='/'>
+        &lt;&lt; Go Back
+    </NavLink>)
